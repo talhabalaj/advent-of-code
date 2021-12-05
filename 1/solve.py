8d5d65ff52e4ef758fcbfe8efdb9516224336a9b
Talha Balaj <talhabalaj@hotmail.com>
2021-12-05 21:57:14 +0500

    Initial commit, solved 1st prog

---
+++ 1/solve.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+data = open('input_data', 'r').read()
+arr = data.split('\n')
+inc_times = 0
+l_arr = len(arr)
+
+for i in range(1, l_arr):
+    if arr[i] == "": continue
+    if int(arr[i]) > int(arr[i - 1]):
+        inc_times += 1
+
+print(inc_times)
+
