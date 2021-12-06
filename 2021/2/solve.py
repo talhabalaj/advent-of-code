34c7e0e1abcd24e1d0df3ba8f4f3f7048c5e7fe4
Talha Balaj <talhabalaj@hotmail.com>
2021-12-06 17:58:14 +0500

    Added more solutions & probs

--- 2021/2/solve.py
+++ 2021/2/solve.py
@@ -1,5 +1,18 @@
-#!/usr/bin/env python3 
+#!/usr/bin/env python3
 data = open('input.txt', 'r').read().splitlines()
 
+hp = 0
+d = 0
+
 for line in data:
-  print(line)
+  inst, val = line.split(' ')
+  val = int(val)
+  print(inst, val)
+  if inst == 'forward':
+    hp += val
+  elif inst == 'up':
+    d -= val
+  elif inst == 'down':
+    d += val
+
+print(hp, d, hp*d)
--- fetch_problem.py
+++ fetch_problem.py
@@ -14,28 +14,31 @@ for env in envs:
 
 parser = argparse.ArgumentParser(
     description="Fetch Advent of Code Problem Data")
-parser.add_argument(
-    '-y', type=int, help="The year of the problem", dest="year",  default=datetime.now().year)
-parser.add_argument('-p', type=int, dest="problem",
-                    help="The problem number", required=True)
+parser.add_argument('-y', type=int, help="The year of the problem",
+                    dest="year",  default=datetime.now().year)
+parser.add_argument('problems', type=int,  nargs='+',
+                    metavar="problem",   help="The list of problem to fetch")
 
 args = parser.parse_args()
 year = args.year
-problem = args.problem
+problems = args.problems
 
-advent_url = "https://adventofcode.com/{}/day/{}/input".format(year, problem)
-data_input = requests.get(
-    advent_url, headers={'cookie': env_map['COOKIE']}).text
 
-os.makedirs(f"{year}/{problem}", exist_ok=True)
+for problem in problems:
+  advent_url = "https://adventofcode.com/{}/day/{}/input".format(year, problem)
+  data_input = requests.get(
+      advent_url, headers={'cookie': env_map['COOKIE']}).text
 
-open(f"{year}/{problem}/input.txt", 'w').write(data_input)
+  os.makedirs(f"{year}/{problem}", exist_ok=True)
 
-if not os.path.exists(f"{year}/{problem}/solve.py"):
-  open(f"{year}/{problem}/solve.py", 'w').write("""
-#!/usr/bin/env python3 
-data = open('input.txt', 'r').read().splitlines()
+  open(f"{year}/{problem}/input.txt", 'w').write(data_input)
 
-for line in data:
-  print(line)
-  """.strip())
+  if not os.path.exists(f"{year}/{problem}/solve.py"):
+    open(f"{year}/{problem}/solve.py", 'w').write("""
+  #!/usr/bin/env python3 
+  data = open('input.txt', 'r').read().splitlines()
+
+  for line in data:
+    print(line)
+    """.strip())
+    os.chmod(f"{year}/{problem}/solve.py", 0o744)
