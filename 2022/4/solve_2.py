deda3c6e847833fef3bb47d3817dd973599f18c0
Talha Balaj <talhabalaj@hotmail.com>
2022-12-04 12:37:30 +0500

    Add solution to problem #4

---
+++ 2022/4/solve_2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+data = open('input.txt', 'r').read().splitlines()
+
+overlaps = 0
+
+for line in data:
+  range_1, range_2 = line.split(',')
+  range_1_start, range_1_end = range_1.split('-')
+  range_2_start, range_2_end = range_2.split('-')
+
+  range_1_start = int(range_1_start)
+  range_1_end = int(range_1_end)
+  range_2_start = int(range_2_start)
+  range_2_end = int(range_2_end)
+
+  if range_1_start <= range_2_start and range_1_end >= range_2_start:
+    overlaps += 1
+  elif range_2_start <= range_1_start and range_2_end >= range_1_start:
+    overlaps += 1
+
+print(overlaps)
